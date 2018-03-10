7a8d6823105f08c23bcd24c456d48640886fd53b
mystic997 <pkrisztian997@gmail.com>
2018-03-10 17:47:18 +0100

    GUI

---
+++ YachtKlub/YachtKlub/PersonalAdminWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YachtKlub
+{
+    /// <summary>
+    /// Interaction logic for PersonalAdminWindow.xaml
+    /// </summary>
+    public partial class PersonalAdminWindow : Window
+    {
+        public PersonalAdminWindow(string LoginNev)
+        {
+            InitializeComponent();
+        }
+
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ YachtKlub/YachtKlub/database/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YachtKlub.database
+{
+    class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("YachtKlubDB")
+        {
+            /*Database.Delete();
+            Database.Create();*/
+            Database.SetInitializer(new DropCreateDatabaseAlways<DatabaseContext>());
+        }
+
+        public DbSet<Members> Members { get; set; }
+        public DbSet<Boats> Boats { get; set; }
+    }
+}
