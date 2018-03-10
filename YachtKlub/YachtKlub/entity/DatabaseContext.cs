6001c3678ccc5651ea2b9983dbf768ab2bec1171
mystic997 <pkrisztian997@gmail.com>
2018-03-10 21:38:46 +0100

    DatabaseContextEntity renamed to DatabaseContext

---
+++ YachtKlub/YachtKlub/entity/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YachtKlub.entity
+{
+    class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("YachtKlubDB")
+        {
+            /*Database.Delete();
+            Database.Create();*/
+            //Database.Delete();
+            //Database.Create();
+            //Database.SetInitializer(new DropCreateDatabaseAlways<DatabaseContext>());
+        }
+
+        public DbSet<MembersEntity> Members { get; set; }
+        public DbSet<BoatsEntity> Boats { get; set; }
+        public DbSet<BoatRentalsEntity> BoatRentals { get; set; }
+        public DbSet<TransportDevicesEntity> TransportDevices { get; set; }
+        public DbSet<RentRequestsEntity> RentRequests { get; set; }
+
+    }
+}
