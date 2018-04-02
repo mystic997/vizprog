6dbd13efa8a0c474802a665e24cc743157ee0dae
danimgithub <37895151+danimgithub@users.noreply.github.com>
2018-04-02 12:33:45 +0200

    GetTemplate függvények, de még nem teljes

--- YachtKlub/YachtKlub/dao/BoatsDaoImpl.cs
+++ YachtKlub/YachtKlub/dao/BoatsDaoImpl.cs
@@ -36,7 +36,47 @@ namespace YachtKlub.dao
 
         public List<BoatsEntity> GetTemplateBoats()
         {
-            throw new NotImplementedException();
+            Random random = new Random();
+
+            List<BoatsEntity> TemplateBoats = new List<BoatsEntity>();
+
+            for (int i = 0; i < 12; i++)
+            {
+
+            BoatsEntity TemplateBoat = new BoatsEntity();
+
+                TemplateBoat.BoatId = i;
+                TemplateBoat.BoatName = "BoatName" + i;
+                TemplateBoat.BoatType = "BoatType" + random.Next(0, 4);
+                TemplateBoat.DailyPrice = random.Next(25000, 15000);
+
+                MembersDao mem = new MembersDaoImpl();
+                List<MembersEntity> mems = mem.GetTemplateMembers();
+
+                TemplateBoat.FKOwner = mems[random.Next(0, mems.Count)];
+
+                TemplateBoat.WhereIsNowTheBoat = "Place" + random.Next(0, 7);
+
+                TemplateBoat.IsLoan = "true";
+
+                TemplateBoat.MaxPerson = random.Next(1, 15);
+                TemplateBoat.MaxSpeed = random.Next(5, 50);//knots
+                TemplateBoat.DiveDepth = random.Next(100, 300) / 100;
+                TemplateBoat.Consumption = random.Next(100, 500) / 100;
+                TemplateBoat.YearOfManufacture = new DateTime(2018, 4, 2);
+                TemplateBoat.BoatLength = random.Next(150, 700) / 100;
+                TemplateBoat.BoatWidth = random.Next(150, 400) / 100;
+                TemplateBoat.BoatImage = "BoatImage" + i;
+
+                TemplateBoat.BoatRentals = new List<BoatRentalsEntity>();//Ez csak ideiglenes
+
+                TemplateBoat.RentRequests = new List<RentRequestsEntity>();//Ez csak ideiglenes
+
+                TemplateBoats.Add(TemplateBoat);
+            }
+
+            return TemplateBoats;
+
         }
     }
 }
--- YachtKlub/YachtKlub/dao/TransportDevicedImpl.cs
+++ YachtKlub/YachtKlub/dao/TransportDevicedImpl.cs
@@ -25,35 +25,33 @@ namespace YachtKlub.dao
 
         public List<TransportDevicesEntity> GetTemplateTransportDevices()
         {
-            //List<TransportDevicesEntity> TransportDevices = new List<TransportDevicesEntity>();
+            List<TransportDevicesEntity> TransportDevices = new List<TransportDevicesEntity>();
 
-            //for (int i = 0; i < 12; i++)
-            //{
-            //    Random random = new Random();
+            for (int i = 0; i < 12; i++)
+            {
+                Random random = new Random();
 
-            //    TransportDevicesEntity TemplateTransportDevice = new TransportDevicesEntity();
+                TransportDevicesEntity TemplateTransportDevice = new TransportDevicesEntity();
 
-            //    TemplateTransportDevice.TransportDeviceId = i;
-            //    TemplateTransportDevice.TransportDeviceName = "TransportDevice" + i;
-            //    TemplateTransportDevice.CarryingCapacity = random.Next(1, 50);
-            //    TemplateTransportDevice.TransportDeviceType = "Type" + random.Next(0, 3);
-            //    TemplateTransportDevice.TransportDeviceLength = random.Next(100, 700) / 100;
-            //    TemplateTransportDevice.TransportDeviceWidth = random.Next(50, 300) / 100;
-            //    TemplateTransportDevice.TransportDeviceImage = "TransportDevice" + i;
+                TemplateTransportDevice.TransportDeviceId = i;
+                TemplateTransportDevice.TransportDeviceName = "TransportDevice" + i;
+                TemplateTransportDevice.CarryingCapacity = random.Next(1, 50);
+                TemplateTransportDevice.TransportDeviceType = "Type" + random.Next(0, 3);
+                TemplateTransportDevice.TransportDeviceLength = random.Next(100, 700) / 100;
+                TemplateTransportDevice.TransportDeviceWidth = random.Next(50, 300) / 100;
+                TemplateTransportDevice.TransportDeviceImage = "TransportDevice" + i;
 
-            //    MembersDao mem = new MembersDaoImpl();
-            //    List<MembersEntity> mems = mem.GetTemplateMembers();
 
-            //    TemplateTransportDevice.FKOwner = mems[random.Next(0, mems.Count)];
+                TemplateTransportDevice.FKOwner = new MembersEntity();//Ez csak ideiglenes
 
-            //    TemplateTransportDevice.BoatRentals = new List<BoatRentalsEntity>();//Ez csak ideiglenes
-            //    TemplateTransportDevice.RentRequests = new List<RentRequestsEntity>();//Ez csak ideiglenes
+                TemplateTransportDevice.BoatRentals = new List<BoatRentalsEntity>();//Ez csak ideiglenes
+                TemplateTransportDevice.RentRequests = new List<RentRequestsEntity>();//Ez csak ideiglenes
 
-            //    TransportDevices.Add(TemplateTransportDevice);
-
-            //}
-            //return TransportDevices;
-            throw new NotImplementedException();
+                TransportDevices.Add(TemplateTransportDevice);
+                
+            }
+            return TransportDevices;
+            //throw new NotImplementedException();
         }
 
         public List<TransportDevicesEntity> GetAllTransportDevicesByOwner(MembersEntity Owner)/*Függvény, ami visszaad egy listát, ami tartalmazza, hogy az adott felhasználónak, milyen szállító eszközei vannak*/
