7ec19f9afe9ad2d8ff980660ed632e8ab53350c1
danimgithub <37895151+danimgithub@users.noreply.github.com>
2018-04-02 19:00:31 +0200

    Dani #11
    Passwordchange

--- YachtKlub/YachtKlub/PasswordChangeWindow.xaml.cs
+++ YachtKlub/YachtKlub/PasswordChangeWindow.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using YachtKlub.dao;
+using YachtKlub.entity;
+using YachtKlub.service;
+using YachtKlub.validator;
 
 namespace YachtKlub
 {
@@ -19,14 +23,45 @@ namespace YachtKlub
     /// </summary>
     public partial class PasswordChangeWindow : Window
     {
+        private string Email { get; set; }
+            
         public PasswordChangeWindow(string email)
         {
             InitializeComponent();
+            Email = email;
         }
 
         private void btCancel_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void btSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string oldPassword = tbOldPassword.Text;
+                string password = tbPassword.Text;
+                string passwordAgain = tbPasswordAgain.Text;
+
+                Validator PasswordChangeValidator = new Validator();
+
+                PasswordChangeValidator.ValidationComponents.Add(new EmptyFieldValidator(oldPassword, "régi jelszó"));
+                PasswordChangeValidator.ValidationComponents.Add(new EmptyFieldValidator(password, "új jelszó"));
+                PasswordChangeValidator.ValidationComponents.Add(new EmptyFieldValidator(passwordAgain, "új jelszó megerősítése"));
+
+                PasswordChangeValidator.ValidationComponents.Add(new SameFieldValidator(password, passwordAgain, "jelszó"));
+
+                // kivetelt dob, ha a validalas hibara fut, egyuttal ki is irja a hibauzeneteket
+                PasswordChangeValidator.ValidateElements();
+                // kivetelt dob, ha sikertelen a service, egyuttal ki is irja a hibauzeneteket
+                PasswordChangeService passwordChangeService = new PasswordChangeService(oldPassword, password, Email);
+                if (passwordChangeService.ServiceStatus == Status.OK)
+                {
+                    Close();
+                }
+            }
+            catch (Exception ex) { }
         }
     }
 }
---
+++ YachtKlub/YachtKlub/service/PasswordChangeService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YachtKlub.dao;
+using YachtKlub.entity;
+using YachtKlub.validator;
+
+namespace YachtKlub.service
+{
+    class PasswordChangeService : ServiceResponse
+    {
+        public string OldPassword { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+
+        public PasswordChangeService(string oldPassword, string password, string email)
+        {
+            OldPassword = oldPassword;
+            Password = password;
+            Email = email;
+            TryToChangePassword();
+
+
+
+        }
+
+        private void TryToChangePassword()
+        {
+            MembersDao membersDao = new MembersDaoImpl();
+            MembersEntity member = membersDao.getMemberByEmail(Email);
+
+            if (!member.Password.Equals(Password))
+            {
+                FeedbackMessage = "Hibásan adta meg a régi  jelszót!";
+                ServiceStatus = Status.Error;
+            }
+            else
+            {
+                member.Password = Password;
+                DatabaseContext dbc = new DatabaseContext();
+                dbc.SaveChanges();
+                FeedbackMessage = "Sikeres jelszó változtatás!";
+                ServiceStatus = Status.OK;
+            }
+
+            // it must be a method
+            if (!string.IsNullOrEmpty(FeedbackMessage) && !string.IsNullOrWhiteSpace(FeedbackMessage))
+            {
+                new PrintMessageBox(FeedbackMessage, ServiceStatus);
+            }
+
+        }
+    }
+}
