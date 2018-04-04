fb1d9620ad37a678e91d0bf4f28dde41ce10bd6c
Kobcy <krisztiankoblos@gmail.com>
2018-04-04 18:39:23 +0200

    Foglalás ablak + UserControl + gomb PersonalWindow
    Booking.xaml --> Foglalás ablak: hajók kiválasztása (beágyazott UC), hajó adatok, tulajdonos adatok, kezdeti+vége dátum, foglalás megerősítése gomb, mégsem gomb
    BookableBoatsUC.xaml --> UserControl: A lefoglalható hajók kilistázásához
    Personal[Admin]Window --> Mindkét felhasználói felülethez hozzáadva egy Foglalás és egy Saját hajóim/eszközeim gomb

---
+++ YachtKlub/YachtKlub/Booking.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YachtKlub
+{
+    /// <summary>
+    /// Interaction logic for Booking.xaml
+    /// </summary>
+    public partial class Booking : Window
+    {
+        public Booking()
+        {
+            InitializeComponent();
+        }
+
+        private void btBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btBook_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
