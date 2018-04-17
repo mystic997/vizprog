fa72566e333f9d13139e723f1152e108bbe129a7
danimgithub <37895151+danimgithub@users.noreply.github.com>
2018-04-17 20:45:12 +0200

    UpdateBoatDataService megírva

---
+++ YachtKlub/YachtKlub/service/UpdateBoatDataService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YachtKlub.dao;
+using YachtKlub.entity;
+using YachtKlub.validator;
+
+namespace YachtKlub.service
+{
+    class UpdateBoatDataService : ServiceResponse
+    {
+        private int BoatId;
+        private string BoatName;
+        private string BoatType;
+        private double DailyPrice;
+        private string WhereIsNowTheBoat;
+        private string IsLoan;
+        private int MaxPerson;
+        private double MaxSpeed;
+        private double DiveDepth;
+        private double Consumption;
+        private DateTime YearOfManufacture;
+        private double BoatLength;
+        private double BoatWidth;
+        private string BoatImage;
+        DatabaseContext dbc;
+
+
+        public UpdateBoatDataService(int BoatId, string BoatName, string BoatType, double DailyPrice, string WhereIsNowTheBoat, string IsLoan, int MaxPerson, double MaxSpeed, double DiveDepth, double Consumption, DateTime YearOfManufacture, double BoatLength, double BoatWidth, string BoatImage)
+        {
+            this.BoatId = BoatId;
+            this.BoatName = BoatName;
+            this.BoatType = BoatType;
+            this.DailyPrice = DailyPrice;
+            this.WhereIsNowTheBoat = WhereIsNowTheBoat;
+            this.IsLoan = IsLoan;
+            this.MaxPerson = MaxPerson;
+            this.MaxSpeed = MaxSpeed;
+            this.DiveDepth = DiveDepth;
+            this.Consumption = Consumption;
+            this.YearOfManufacture = YearOfManufacture;
+            this.BoatLength = BoatLength;
+            this.BoatWidth = BoatWidth;
+            this.BoatImage = BoatImage;
+
+            dbc = AliveContext.Context;
+
+            UpdateBoat();
+
+        }
+
+        private void UpdateBoat()
+        {
+            BoatsEntity boatData  = dbc.Boats.SingleOrDefault(m => m.BoatId.Equals(BoatId));
+            boatData.BoatId = this.BoatId;
+            boatData.BoatName = this.BoatName;
+            boatData.BoatType = this.BoatType;
+            boatData.DailyPrice = this.DailyPrice;
+            boatData.WhereIsNowTheBoat = this.WhereIsNowTheBoat;
+            boatData.IsLoan = this.IsLoan;
+            boatData.MaxPerson = this.MaxPerson;
+            boatData.MaxSpeed = this.MaxSpeed;
+            boatData.DiveDepth = this.DiveDepth;
+            boatData.Consumption = this.Consumption;
+            boatData.YearOfManufacture = this.YearOfManufacture;
+            boatData.BoatLength = this.BoatLength;
+            boatData.BoatWidth = this.BoatWidth;
+            boatData.BoatImage = this.BoatImage;
+
+            dbc.SaveChanges();
+
+            FeedbackMessage = "Adatok sikeresen módosítva!";
+            ServiceStatus = Status.OK;
+
+            // it must be a method
+            if (!string.IsNullOrEmpty(FeedbackMessage) && !string.IsNullOrWhiteSpace(FeedbackMessage))
+            {
+                new PrintMessageBox(FeedbackMessage, ServiceStatus);
+            }
+
+        }
+    }
+}
