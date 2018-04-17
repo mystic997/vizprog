b05debb6da18b07792c71e34fe7786572dec0d6b
danimgithub <37895151+danimgithub@users.noreply.github.com>
2018-04-17 23:15:03 +0200

    további Gui finomítások

--- YachtKlub/YachtKlub/NewBoatOrTransportDeviceWindow.xaml.cs
+++ YachtKlub/YachtKlub/NewBoatOrTransportDeviceWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,25 +20,47 @@ namespace YachtKlub
     /// </summary>
     public partial class NewBoatWindow : Window
     {
-        List<object> ShouldBeHiddenSometimes;
+        List<Control> ShouldBeHiddenSometimes;
         public NewBoatWindow(bool boat)
         {
             InitializeComponent();
-            ShouldBeHiddenSometimes = new List<object>();
+            ShouldBeHiddenSometimes = new List<Control>();
 
             ShouldBeHiddenSometimes.Add(lbBoatDept);
-            /*folytatom, Dani*/
+            ShouldBeHiddenSometimes.Add(tbBoatDept);
+            ShouldBeHiddenSometimes.Add(lbDepth);
+            ShouldBeHiddenSometimes.Add(lbSpeed);
+            ShouldBeHiddenSometimes.Add(tbBoatSpeed);
+            ShouldBeHiddenSometimes.Add(lbBoatSpeed);
+            //ShouldBeHiddenSometimes.Add(lbBoatManpower);
+            //ShouldBeHiddenSometimes.Add(tbBoatManpower);
+            //ShouldBeHiddenSometimes.Add(lbManpower);
+            ShouldBeHiddenSometimes.Add(tbBoatPlace);
+            ShouldBeHiddenSometimes.Add(lbBoatPlace);
+            ShouldBeHiddenSometimes.Add(tbBoatYear);
+            ShouldBeHiddenSometimes.Add(lbBoatYear);
+            ShouldBeHiddenSometimes.Add(lbConsumption);
+            ShouldBeHiddenSometimes.Add(tbBoatConsumption);
+            ShouldBeHiddenSometimes.Add(lbBoatConsumption);
+            ShouldBeHiddenSometimes.Add(lbBoatPrice);
+            ShouldBeHiddenSometimes.Add(tbBoatPrice);
+            ShouldBeHiddenSometimes.Add(lbPrice);
+
+
+
 
             if (boat)
             {
-                
+
             }
             else
             {
+                lbBoatManpower.Content = "Teherbírás";
+                lbManpower.Content = "kg";
+                ShouldBeHiddenSometimes.ForEach(i => i.Visibility = Visibility.Hidden);
                 lbHeader.Content = "Új szállítóeszköz regisztrálása";
                 lbHeader2.Content = "A szállítóeszköz adatai";
-                lbBoatDept.Visibility = Visibility.Hidden;
-                tbBoatDept.Visibility = Visibility.Hidden;
+
 
             }
 
@@ -50,16 +73,30 @@ namespace YachtKlub
 
         private void UploadPic_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            dlg.DefaultExt = ".jpg";
-            dlg.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" + "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" + "Portable Network Graphic (*.png)|*.png";
-            Nullable<bool> result = dlg.ShowDialog();
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+
+            dialog.DefaultExt = ".jpg";
+            dialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" + "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" + "Portable Network Graphic (*.png)|*.png";
+
+            Nullable<bool> result = dialog.ShowDialog();
 
             if (result == true)
             {
                 // Open document
-                string filename = dlg.FileName;
+                imgBoatPicture.Tag = dialog.FileName;
+                System.IO.Directory.CreateDirectory(System.AppDomain.CurrentDomain.BaseDirectory + "\\" + "resources");
+                string fileName = System.IO.Path.GetFileName(Convert.ToString(imgBoatPicture.Tag));
+                string newFileName = generateID() + ".jpg";
+                File.Copy(Convert.ToString(imgBoatPicture.Tag), System.AppDomain.CurrentDomain.BaseDirectory + "\\" + "resources" + "\\" + newFileName, true);
+                imgBoatPicture.Tag = System.AppDomain.CurrentDomain.BaseDirectory + "\\" + "resources" + "\\" + newFileName;
+                var uri = new Uri(System.AppDomain.CurrentDomain.BaseDirectory + "\\" + "resources" + "\\" + newFileName, UriKind.Absolute);
+                var bitmap = new BitmapImage(uri);
+                imgBoatPicture.Source = bitmap;
             }
+        }
+        public string generateID()
+        {
+            return Guid.NewGuid().ToString("N");
         }
     }
 }
