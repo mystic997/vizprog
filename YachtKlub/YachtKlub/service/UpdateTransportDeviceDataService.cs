644a5825ddb61e3a339a422ac04e8fe659cfac44
danimgithub <37895151+danimgithub@users.noreply.github.com>
2018-04-17 21:42:51 +0200

    UpdateTransportDeviceDataService megírva :chicken:

---
+++ YachtKlub/YachtKlub/service/UpdateTransportDeviceDataService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YachtKlub.dao;
+using YachtKlub.entity;
+using YachtKlub.validator;
+
+namespace YachtKlub.service
+{
+    class UpdateTransportDeviceDataService
+    {
+
+        private int TransportDeviceId;
+        private string TransportDeviceName;
+        private double CarryingCapacity;
+        private string TransportDeviceType;
+        private double TransportDeviceLength;
+        private double TransportDeviceWidth;
+        private string TransportDeviceImage;
+        DatabaseContext dbc;
+
+
+        UpdateTransportDeviceDataService(
+            int TransportDeviceId,
+            string TransportDeviceName,
+            double CarryingCapacity,
+            string TransportDeviceType,
+            double TransportDeviceLength,
+            double TransportDeviceWidth,
+            string TransportDeviceImage)
+        {
+            this.TransportDeviceId = TransportDeviceId;
+            this.TransportDeviceName = TransportDeviceName;
+            this.CarryingCapacity = CarryingCapacity;
+            this.TransportDeviceType = TransportDeviceType;
+            this.TransportDeviceLength = TransportDeviceLength;
+            this.TransportDeviceWidth = TransportDeviceWidth;
+            this.TransportDeviceImage = TransportDeviceImage;
+
+            dbc = AliveContext.Context;
+            UpdateTransportDeviceData();
+        }
+
+        public void UpdateTransportDeviceData()
+        {
+            TransportDevicesEntity transportDeviceData = dbc.TransportDevices.SingleOrDefault(m => m.TransportDeviceId.Equals(TransportDeviceId));
+
+            transportDeviceData.TransportDeviceId = this.TransportDeviceId;
+              transportDeviceData.TransportDeviceName = this.TransportDeviceName;
+              transportDeviceData.CarryingCapacity = this.CarryingCapacity;
+              transportDeviceData.TransportDeviceType = this.TransportDeviceType;
+              transportDeviceData.TransportDeviceLength = this.TransportDeviceLength;
+              transportDeviceData.TransportDeviceWidth = this.TransportDeviceWidth;
+              transportDeviceData.TransportDeviceImage = this.TransportDeviceImage;
+        }
+
+    }
+}
