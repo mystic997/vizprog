98bd5b908ea481685688edda245a556929b8ffdd
danimgithub <37895151+danimgithub@users.noreply.github.com>
2018-04-22 16:29:19 +0200

    Revert "Revert "Dani""
    This reverts commit f2e90795ddf256c1c4f2510ea9a5e37a77ee48ef.

---
+++ YachtKlub/YachtKlub/StatisicsWindow1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YachtKlub
+{
+    /// <summary>
+    /// Interaction logic for StatisicsWindow1.xaml
+    /// </summary>
+    public partial class StatisicsWindow1 : Window
+    {
+        public StatisicsWindow1()
+        {
+            InitializeComponent();
+            towerChart(20, 550, 30);
+
+        }
+
+        public void towerChart(double x, double y, double height)
+        {
+            Rectangle tower = new Rectangle();
+            tower.Width = 20;
+            tower.Height = height;
+            tower.Stroke = Brushes.Azure;
+            tower.Fill = Brushes.Azure;
+            tower.StrokeThickness = 2;
+            tower.SetValue(Canvas.LeftProperty, (double)(x));
+            tower.SetValue(Canvas.TopProperty, (double)(y));
+            cvLap.Children.Add(tower);
+
+        }
+    }
+}
